3640087c75f5de9e7cb9e145aba653fb43f08a15
Andrew Kushnir <akushnir2009@gmail.com>
2020-07-08 00:05:18 -0600

    Stock price

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,23 @@
 module ApplicationHelper
+
+  def delta_class(number)
+    number = number.to_s
+    if number.blank? || !(number =~ /[1-9]/i)
+      'text-muted'
+    elsif number =~ /-/i
+      'text-danger'
+    else
+      'text-success'
+    end
+  end
+
+  def delta_number(number)
+    number = number.to_s
+    if number.present? && number =~ /[1-9]/i && !(number =~ /-/i)
+      "+#{number}"
+    else
+      number
+    end
+  end
+
 end

09c8ec97404cfb9dfded8b3b267c9ca10b098ef3
Andrew Kushnir <akushnir2009@gmail.com>
2020-07-10 03:22:05 -0600

    Re-factoring

--- app/models/position.rb
+++ app/models/position.rb
@@ -6,6 +6,8 @@ class Position < ApplicationRecord
   validates :user, presence: true
   validates :stock, presence: true, uniqueness: { scope: :user }
 
+  before_save :recalculate
+
   def to_s
     stock&.to_s
   end
@@ -14,6 +16,16 @@ class Position < ApplicationRecord
     model = where(stock: stock)
     model.update_all(market_price: stock.current_price)
     model.update_all('market_value = shares * market_price')
+    model.update_all('gain_loss = market_value - total_cost')
+    model.update_all('gain_loss_pct = gain_loss / total_cost * 100')
+  end
+
+  def recalculate
+    self.total_cost = average_price * shares rescue nil
+    self.market_price = stock.current_price rescue nil
+    self.market_value = market_price * shares rescue nil
+    self.gain_loss = market_value - total_cost rescue nil
+    self.gain_loss_pct = gain_loss / total_cost * 100 rescue nil
   end
 
 end

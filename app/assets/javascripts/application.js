594c3cd4649c4e8417abd94dd6a73e5d63dee04c
Andrew Kushnir <akushnir2009@gmail.com>
2020-07-15 02:58:14 -0600

    Dividend Calendar

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -4,3 +4,12 @@
 $(document).on('turbolinks:load', function() {
     $(window).trigger('load.bs.select.data-api');
 });
+
+function formatCurrency(total) {
+    var neg = false;
+    if(total < 0) {
+        neg = true;
+        total = Math.abs(total);
+    }
+    return (neg ? "-$" : '$') + parseFloat(total, 10).toFixed(2).replace(/(\d)(?=(\d{3})+\.)/g, "$1,").toString();
+}

8a078813457838d9c41b0280526940236a2c7ba1
Andrew Kushnir <akushnir2009@gmail.com>
2020-07-17 03:13:32 -0600

    Charts

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -19,3 +19,32 @@ require("packs/data_refresh");
 require("packs/data_table");
 require("packs/twitter");
 require("packs/charts");
+
+
+window.init_representation = function(table, charts, table_radio, charts_radio) {
+    if (location.hash === "#charts") {
+        switch_representation_to_charts(table, charts);
+        $(charts_radio).prop('checked', true);
+    } else {
+        switch_representation_to_table(table, charts);
+        $(table_radio).prop('checked', true);
+    }
+}
+
+window.switch_representation_to_table = function(table, charts) {
+    table = $(table);
+    charts = $(charts);
+    if ($(table).length === 0 || $(charts).length === 0) return;
+    table.show();
+    charts.hide();
+    location.hash = "#table";
+}
+
+window.switch_representation_to_charts = function(table, charts) {
+    table = $(table);
+    charts = $(charts);
+    if ($(table).length === 0 || $(charts).length === 0) return;
+    table.hide();
+    charts.show();
+    location.hash = "#charts";
+}

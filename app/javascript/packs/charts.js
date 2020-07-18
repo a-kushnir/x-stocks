e4e314353eee22f302c8f32fed49c0d85b2c62cf
Andrew Kushnir <akushnir2009@gmail.com>
2020-07-18 07:01:16 -0600

    Charts

--- app/javascript/packs/charts.js
+++ app/javascript/packs/charts.js
@@ -411,3 +411,76 @@ window.allocation_chart = function(canvas, values, labels, symbols = null) {
         }
     });
 }
+
+window.earnings_chart = function(canvas, estimate, actual, labels) {
+    canvas = $(canvas);
+    if (canvas.length === 0) return;
+
+    const config = {
+        type: 'line',
+        data: {
+            labels: labels,
+            datasets: [{
+                label: 'Actual',
+                backgroundColor: function(pointItem) {
+                    const est = pointItem.chart.data.datasets[1].data[pointItem.dataIndex];
+                    const act = pointItem.dataset.data[pointItem.dataIndex];
+                    return est > act ? '#FF333A' : '#00C073';
+                },
+                borderColor: function(pointItem) {
+                    const est = pointItem.chart.data.datasets[1].data[pointItem.dataIndex];
+                    const act = pointItem.dataset.data[pointItem.dataIndex];
+                    return est > act ? '#FF333A' : '#00C073';
+                },
+                showLine: false,
+                pointRadius: 8,
+                pointHoverRadius: 9,
+                pointBorderWidth: 2,
+                pointHoverBorderWidth: 2,
+                data: actual,
+                fill: false,
+            }, {
+                label: 'Estimate',
+                fill: false,
+                pointHoverBackgroundColor: 'white',
+                backgroundColor: 'white',
+                borderColor: '#0F69FF',
+                showLine: false,
+                pointRadius: 8,
+                pointHoverRadius: 9,
+                pointBorderWidth: 2,
+                pointHoverBorderWidth: 2,
+                data: estimate,
+            }]
+        },
+        options: {
+            responsive: true,
+            legend: {
+                display: false
+            },
+            tooltips: {
+                mode: 'index',
+                intersect: false,
+            },
+            hover: {
+                mode: 'nearest',
+                intersect: true
+            },
+            scales: {
+                xAxes: [{
+                    display: true,
+                }],
+                yAxes: [{
+                    display: true,
+                }]
+            },
+            plugins: {
+                datalabels: {
+                    display: false,
+                }
+            }
+        }
+    };
+
+    new Chart(canvas, config);
+}

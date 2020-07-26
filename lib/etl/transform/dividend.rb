2ddaa1fa94fa20c285ede646d0d5b34c03366b04
Andrew Kushnir <akushnir2009@gmail.com>
2020-07-26 16:08:57 -0600

    Virtual Tags

--- lib/etl/transform/dividend.rb
+++ lib/etl/transform/dividend.rb
@@ -11,37 +11,7 @@ module Etl
         stock.dividend_growth_3y = number_or_nil(node['growth_over_years'])
         stock.dividend_growth_years = number_or_nil(node['consective_year_of_growth'])
 
-        if stock.save
-
-          if number_or_nil(node['consective_year_of_growth']).is_a?(Numeric)
-            tags = []
-
-            if stock.dividend_growth_years >= 10
-              tags << 'Dividend Achiever'
-            end
-            if stock.dividend_growth_years >= 10 && stock.dividend_growth_years < 25
-              tags << 'Dividend Contender'
-            end
-            if stock.dividend_growth_years >= 25
-              tags << 'Dividend Champion'
-            end
-
-            ::Tag.batch_update(stock, :dividend_tag, tags)
-          end
-
-          if node['dars_overall'].present?
-            tags = []
-            tags << 'Safe Dividends' if stock.dividend_rating && stock.dividend_rating >= 4.0
-            ::Tag.batch_update(stock, :safe_dividend_tag, tags)
-          end
-
-          if stock.dividend_growth_3y.to_f >= 5.0 || stock.dividend_growth_5y.to_f >= 5.0
-            tags = []
-            tags << 'High Dividend Growth'
-            ::Tag.batch_update(stock, :dividend_growth_tag, tags)
-          end
-
-        end
+        stock.save
       end
 
     end
--- lib/virtual_tag.rb
+++ lib/virtual_tag.rb
@@ -29,6 +29,11 @@ class VirtualTag
   private
 
   ALL = [
+      VirtualTag.new('Dividend Achiever', 'Dividend Achievers (≥10 years)', ->(position) { position.stock.dividend_growth_years && position.stock.dividend_growth_years >= 10 }, ->(user) { Stock.where('dividend_growth_years >= 10') } ),
+      VirtualTag.new('Dividend Contender', 'Dividend Contenders (10-24 years)', ->(position) { position.stock.dividend_growth_years && position.stock.dividend_growth_years >= 10 && position.stock.dividend_growth_years < 25 }, ->(user) { Stock.where('dividend_growth_years >= 10 and dividend_growth_years < 25') } ),
+      VirtualTag.new('Dividend Champion', 'Dividend Champions (≥25 years)', ->(position) { position.stock.dividend_growth_years && position.stock.dividend_growth_years >= 25 }, ->(user) { Stock.where('dividend_growth_years >= 25') } ),
+      VirtualTag.new('Safe Dividend', 'Safe Dividends (Rating ≥4.0)', ->(position) { position.stock.dividend_rating && position.stock.dividend_rating >= 4.0 }, ->(user) { Stock.where('dividend_rating >= 4.0') } ),
+      VirtualTag.new('High Dividend Growth', 'High Dividend Growth (≥5.0%)', ->(position) { position.stock.dividend_growth_3y.to_f >= 5.0 || position.stock.dividend_growth_5y.to_f >= 5.0 }, ->(user) { Stock.where('dividend_growth_3y >= 5.0 or dividend_growth_5y >= 5.0') } ),
       VirtualTag.new('S&P 500', 'S&P 500 Index', ->(position) { position.stock.sp500 }, ->(user) { Stock.where(sp500: true) } ),
       VirtualTag.new('Nasdaq 100', 'Nasdaq 100 Index', ->(position) { position.stock.nasdaq100 }, ->(user) { Stock.where(nasdaq100: true) } ),
       VirtualTag.new('Dow Jones', 'Dow Jones Industrial Average', ->(position) { position.stock.dowjones }, ->(user) { Stock.where(dowjones: true) } ),

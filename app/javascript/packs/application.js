3366e7bff7f508c2da53ed4ad26c9576ab8096cd
Andrew Kushnir <akushnir2009@gmail.com>
2020-07-25 22:11:17 -0600

    Xlsx

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -25,10 +25,10 @@ require("packs/charts");
 window.init_representation = function(table, charts, table_radio, charts_radio) {
     if (location.hash === "#charts") {
         switch_representation_to_charts(table, charts);
-        $(charts_radio).prop('checked', true);
+        $(charts_radio).parents('label').button('toggle');
     } else {
         switch_representation_to_table(table, charts);
-        $(table_radio).prop('checked', true);
+        $(table_radio).parents('label').button('toggle');
     }
 }
 
@@ -39,6 +39,22 @@ window.switch_representation_to_table = function(table, charts) {
     table.show();
     charts.hide();
     location.hash = "#table";
+}
+
+window.switch_representation_to_excel = function(table, charts, table_radio, charts_radio) {
+    table = $(table);
+    charts = $(charts);
+    if ($(table).length === 0 || $(charts).length === 0) return;
+
+    setTimeout(function() {
+        if (location.hash === "#charts") {
+            $(charts_radio).parents('label').button('toggle');
+        } else {
+            $(table_radio).parents('label').button('toggle');
+        }
+    },100);
+    
+    location.href = '/dividends.xlsx'
 }
 
 window.switch_representation_to_charts = function(table, charts) {

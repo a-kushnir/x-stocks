867e45a073722393d3e1c8951ef58bafb0d04c9f
Andrew Kushnir <akushnir2009@gmail.com>
2020-07-29 14:46:31 -0600

    Services overhaul

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -1,7 +1,7 @@
 class ServicesController < ApplicationController
 
   def index
-    @services = SERVICES
+    @service_runners = ServiceRunner.all
     @stocks = Stock.all
 
     @page_title = 'Services'
@@ -12,24 +12,22 @@ class ServicesController < ApplicationController
     @page_title = 'Services'
     @page_menu_item = :services
 
-    find_service do |service|
-      service[:proc].call(params)
-      flash[:notice] = "#{service[:name]} complete"
+    find_service_runner do |service_runner|
+      service_runner.run(params)
+      flash[:notice] = "#{service_runner.name} complete"
       redirect_to action: 'index'
     end
   end
 
   def log
-    find_service do |service|
-      serv = service[:service] ? Service.find_by(key: service[:service]) : nil
-      send_data(serv&.log, filename: 'service-log.txt', type: 'text/plain')
+    find_service_runner do |service_runner|
+      send_data(service_runner.service&.log, filename: 'service-log.txt', type: 'text/plain')
     end
   end
 
   def error
-    find_service do |service|
-      serv = service[:service] ? Service.find_by(key: service[:service]) : nil
-      send_data(serv&.error, filename: 'service-error.txt', type: 'text/plain')
+    find_service_runner do |service_runner|
+      send_data(service_runner.service&.error, filename: 'service-error.txt', type: 'text/plain')
     end
   end
 
@@ -53,8 +51,8 @@ class ServicesController < ApplicationController
 
   private
 
-  def find_service
-    service = SERVICES[params[:id]]
+  def find_service_runner
+    service = ServiceRunner.find(params[:id])
     if service
       yield service
     else
@@ -62,102 +60,4 @@ class ServicesController < ApplicationController
       redirect_to action: 'index'
     end
   end
-
-  SERVICES = HashWithIndifferentAccess.new({
-      hourly_all_finnhub: {
-          service: 'stock_prices',
-          name: 'Update stock prices [Finnhub]',
-          schedule: 'Hourly',
-          proc: ->(args) do
-            Etl::Refresh::Finnhub.new.hourly_all_stocks!
-          end
-      },
-      hourly_one_finnhub: {
-          name: 'Update stock prices [Finnhub]',
-          args: [:stock_id],
-          proc: ->(args) do
-            stock = Stock.find_by!(id: args[:stock_id])
-            Etl::Refresh::Finnhub.new.hourly_one_stock!(stock)
-          end
-      },
-      daily_all_yahoo: {
-          service: 'daily_yahoo',
-          name: 'Update stock information [Yahoo]',
-          schedule: 'Daily',
-          proc: ->(args) do
-            Etl::Refresh::Yahoo.new.daily_all_stocks!
-          end
-      },
-      daily_one_yahoo: {
-          name: 'Update stock information [Yahoo]',
-          args: [:stock_id],
-          proc: ->(args) do
-            stock = Stock.find_by!(id: args[:stock_id])
-            Etl::Refresh::Yahoo.new.daily_one_stock!(stock)
-          end
-      },
-      daily_all_finnhub: {
-          service: 'daily_finnhub',
-          name: 'Update stock information [Finnhub]',
-          schedule: 'Daily',
-          proc: ->(args) do
-            Etl::Refresh::Finnhub.new.daily_all_stocks!
-          end
-      },
-      daily_one_finnhub: {
-          name: 'Update stock information [Finnhub]',
-          args: [:stock_id],
-          proc: ->(args) do
-            stock = Stock.find_by!(id: args[:stock_id])
-            Etl::Refresh::Finnhub.new.daily_one_stock!(stock)
-          end
-      },
-      weekly_all_iexapis: {
-          service: 'weekly_iexapis',
-          name: 'Update stock dividends [IEX Cloud]',
-          schedule: 'Weekly',
-          proc: ->(args) do
-            Etl::Refresh::Iexapis.new.weekly_all_stocks!
-          end
-      },
-      weekly_one_iexapis: {
-          name: 'Update stock dividends [IEX Cloud]',
-          args: [:stock_id],
-          proc: ->(args) do
-            stock = Stock.find_by!(id: args[:stock_id])
-            Etl::Refresh::Iexapis.new.weekly_one_stock!(stock, nil, immediate: true)
-          end
-      },
-      weekly_all_dividend: {
-          service: 'weekly_dividend',
-          name: 'Update stock dividends [Dividend.com]',
-          schedule: 'Weekly',
-          proc: ->(args) do
-            Etl::Refresh::Dividend.new.weekly_all_stocks!
-          end
-      },
-      weekly_one_dividend: {
-          name: 'Update stock dividends [Dividend.com]',
-          args: [:stock_id],
-          proc: ->(args) do
-            stock = Stock.find_by!(id: args[:stock_id])
-            Etl::Refresh::Dividend.new.weekly_one_stock!(stock)
-          end
-      },
-      company_information: {
-          name: 'Load company information [Finnhub] [IEX Cloud] [Yahoo]',
-          args: [:stock_id],
-          proc: ->(args) do
-            stock = Stock.find_by!(id: args[:stock_id])
-            Etl::Refresh::Company.new.one_stock!(stock)
-          end
-      },
-      slickcharts: {
-          name: 'S&P 500, Nasdaq 100 and Dow Jones [SlickCharts]',
-          proc: ->(args) do
-            Etl::Refresh::Slickcharts.new.all_stocks!
-          end
-      },
-  })
-
 end
--- app/helpers/services_helper.rb
+++ app/helpers/services_helper.rb
@@ -1,7 +1,15 @@
 module ServicesHelper
 
-  def service_path(service_code)
-    "/services/#{URI.escape(service_code)}"
+  def service_path(service_runner)
+    "/services/#{URI.escape(service_runner.lookup_code)}"
+  end
+
+  def service_log_path(service_runner)
+    "/services/#{URI.escape(service_runner.lookup_code)}/log"
+  end
+
+  def service_error_path(service_runner)
+    "/services/#{URI.escape(service_runner.lookup_code)}/error"
   end
 
 end

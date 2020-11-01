48b41de4c30923feb40c676bf4cd3aca1143ce32
Andrew Kushnir <akushnir2009@gmail.com>
2020-11-01 01:06:26 -0600

    Streaming implementation

--- lib/etl/refresh/base.rb
+++ lib/etl/refresh/base.rb
@@ -12,8 +12,9 @@ module Etl
       def each_stock_with_message
         stocks = Stock.random.all.to_a
         stocks.each_with_index do |stock, index|
+          0/0 if index == 7
           yield stock, {
-              message: "Processing #{stock.symbol}",
+              message: "Processing #{stock.symbol} (#{index + 1} out of #{stocks.size})",
               percent: index * 100 / stocks.length
           }
         end

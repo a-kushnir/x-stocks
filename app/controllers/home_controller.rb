d3a66c390dbb8b4b7035bc52cb49a08b39299bca
Andrew Kushnir <akushnir2009@gmail.com>
2020-10-31 20:17:14 -0600

    Streaming test

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,4 +6,15 @@ class HomeController < ApplicationController
     @fear_n_greed_image_url = Etl::Refresh::FearNGreed.new.image_url
   end
 
+  def demo
+    response.headers['Content-Type'] = 'text/event-stream'
+    response.stream.write "="
+    120.times do |i|
+      sleep(1)
+      response.stream.write 'x'
+    end
+    response.stream.write "="
+    response.stream.close
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
   root to: 'home#index'
+  get '/home/demo', to: 'home#demo'
 
   devise_for :users, :controllers => {:registrations => 'registrations'}
 

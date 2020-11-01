75abb4f3db4cee7ca4aa0b5ea0e8a6feb27e5a59
Andrew Kushnir <akushnir2009@gmail.com>
2020-11-01 01:06:07 -0700

    Streaming implementation

--- app/javascript/packs/data_refresh.js
+++ app/javascript/packs/data_refresh.js
@@ -2,12 +2,17 @@ function refresh_status(text) {
     $('#data-refresh-text').html(text);
 }
 
+let updating_prices = false;
 function update_prices() {
+    if (updating_prices) return;
+    updating_prices = true;
+
     const div_selector = $('#data-refresh-div');
     if (div_selector.length === 0) return;
 
     runEventSource('/services/run', {
         closed: function() {
+            updating_prices = false;
             $('#data-refresh-spin').hide();
             refresh_status("Stocks updated");
             div_selector.fadeOut(3000, function() {

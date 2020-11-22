e4b09dba1a2fb7d075434dd3b8a1ef3bf36bba4c
Andrew Kushnir <akushnir2009@gmail.com>
2020-11-22 13:58:50 -0700

    Improved Service logging

--- lib/etl/refresh/fear_n_greed.rb
+++ lib/etl/refresh/fear_n_greed.rb
@@ -3,7 +3,7 @@ module Etl
     class FearNGreed
 
       def image_url(logger = nil)
-        Config.cached(:fear_n_greed_image_url, 1.hours) do
+        Config.cached(:fear_n_greed_image_url, 10.minutes) do
           Etl::Extract::FearNGreed.new(logger: logger).image_url
         end
       end

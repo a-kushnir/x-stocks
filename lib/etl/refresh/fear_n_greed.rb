b2a1452582b30b98861f4ceac5681e55cb5a8bd9
Andrew Kushnir <akushnir2009@gmail.com>
2020-11-22 14:15:48 -0700

    Fear'n'greed image

--- lib/etl/refresh/fear_n_greed.rb
+++ lib/etl/refresh/fear_n_greed.rb
@@ -3,18 +3,26 @@ module Etl
     class FearNGreed
 
       def image_url(logger = nil)
-        Config.cached(:fear_n_greed_image_url, 1.hour) do
-          extractor = Etl::Extract::FearNGreed.new(logger: logger)
-          source_image_url = extractor.image_url
+        stored_image_url = '/img/fear-and-greed.png'
 
-          stored_image_url = '/img/fear-and-greed.png'
-          unless source_image_url.blank?
-            path = "#{Rails.root}/public#{stored_image_url}"
-            extractor.download(path, source_image_url)
+        begin
+          Config.cached(:fear_n_greed_image_url, 1.hour) do
+            extractor = Etl::Extract::FearNGreed.new(logger: logger)
+            source_image_url = extractor.image_url
+
+
+            unless source_image_url.blank?
+              path = "#{Rails.root}/public#{stored_image_url}"
+              extractor.download(path, source_image_url)
+            end
+
+            stored_image_url
           end
-
-          stored_image_url
+        rescue Exception => error
+          logger&.log_error(error)
         end
+
+        stored_image_url
       end
 
     end

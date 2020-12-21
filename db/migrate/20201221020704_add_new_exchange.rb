cc1f50017a7073c6b8b48a2a547bad1eaf77f561
Andrew Kushnir <akushnir2009@gmail.com>
2020-12-20 19:18:16 -0700

    NYSE MKT added

---
+++ db/migrate/20201221020704_add_new_exchange.rb
@@ -0,0 +1,14 @@
+class AddNewExchange < ActiveRecord::Migration[6.0]
+  def change
+    Exchange.create!({
+      name: 'NYSE',
+      code: 'MKT',
+      region: 'United States',
+      iexapis_code: 'NYSE MKT LLC',
+      webull_code: 'AMEX',
+      finnhub_code: 'NYSE MKT LLC',
+      tradingview_code: 'AMEX',
+      dividend_code: 'AMEX'
+    })
+  end
+end

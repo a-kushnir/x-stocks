a47137c2add4708efc9ff7702c226102c486a0da
Andrew Kushnir <akushnir2009@gmail.com>
2021-01-02 00:37:45 -0700

    Stock API

--- app/controllers/api/entities/position.rb
+++ app/controllers/api/entities/position.rb
@@ -1,19 +1,33 @@
 module API
   module Entities
     class Position < API::Entities::Base
-      expose(:symbol) { |model, _| model.stock.symbol }
-      expose(:exchange) { |model, _| model.stock.exchange.code }
-      with_options(format_with: :float) do
-        expose :shares
-        expose :average_price
-        expose :total_cost
-        expose :market_price
-        expose :market_value
-        expose :gain_loss
-        expose :gain_loss_pct
-        expose :est_annual_dividend
-        expose :est_annual_income
+
+      with_options(if: {type: :details}) do
+        expose(:symbol) { |model, _| model.stock.symbol }
+        expose(:exchange) { |model, _| model.stock.exchange.code }
+        with_options(format_with: :float) do
+          expose :shares
+          expose :average_price
+          expose :total_cost
+          expose :market_price
+          expose :market_value
+          expose :gain_loss
+          expose :gain_loss_pct
+          expose :est_annual_dividend
+          expose :est_annual_income
+        end
       end
+
+      with_options(if: {type: :summary}) do
+        with_options(format_with: :float) do
+          expose :total_cost
+          expose :market_value
+          expose :gain_loss
+          expose :gain_loss_pct
+          expose :est_annual_income
+        end
+      end
+
     end
   end
 end
--- app/controllers/api/v1/positions.rb
+++ app/controllers/api/v1/positions.rb
@@ -5,10 +5,25 @@ module API
 
       namespace :positions do
 
-        desc 'Returns all stock position.'
+        desc 'Returns all stock positions.'
         get do
           positions = Position.where(user: current_user).where.not(shares: nil).all
           present positions, with: API::Entities::Position
+        end
+
+        desc 'Returns all stock position summary.'
+        get 'summary' do
+          summary = OpenStruct.new(total_cost: 0, market_value: 0, gain_loss: 0, gain_loss_pct: 0, est_annual_income: 0)
+
+          Position.where(user: current_user).where.not(shares: nil).all.each do |position|
+            summary.total_cost += position.total_cost if position.total_cost
+            summary.market_value += position.market_value if position.market_value
+            summary.gain_loss += position.gain_loss if position.gain_loss
+            summary.est_annual_income += position.est_annual_income if position.est_annual_income
+          end
+          summary.gain_loss_pct = (summary.gain_loss / summary.total_cost).round(4) * 100 rescue nil
+
+          present summary, with: API::Entities::Position, type: :summary
         end
 
         desc 'Returns stock position.'
@@ -20,7 +35,7 @@ module API
           error!('Unknown Symbol', 404) unless stock
           position = Position.where(user: current_user).where.not(shares: nil).where(stock_id: stock.id).first
           error!('Unknown Symbol', 404) unless position
-          present position, with: API::Entities::Position
+          present position, with: API::Entities::Position, type: :details
         end
       end
 

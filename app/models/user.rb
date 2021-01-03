dd8c7ad846e20d4e1f653139b486c999d040bfd1
Andrew Kushnir <akushnir2009@gmail.com>
2021-01-02 19:42:43 -0700

    Migrations fixed

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,6 +14,6 @@ class User < ApplicationRecord
   private
 
   def generate_api_key
-    self.api_key ||= Devise.friendly_token if User.column_names.include?(:api_key)
+    self.api_key ||= Devise.friendly_token if respond_to?(:api_key)
   end
 end

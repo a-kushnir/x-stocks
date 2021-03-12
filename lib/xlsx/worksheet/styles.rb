1f8c9e49cf80cef4f92ea317bfdbeaa8051f34b2
Andrew Kushnir <akushnir2009@gmail.com>
2021-03-11 19:44:20 -0700

    XLSX export

--- lib/xlsx/worksheet/styles.rb
+++ lib/xlsx/worksheet/styles.rb
@@ -7,10 +7,10 @@ module XLSX
       BASE_FORMAT = { font_name: 'Calibri' }.freeze
       HEADER_FORMAT = { bg_color: 'F2F2F2', b: true }.freeze
       US_DOLLAR_2_DECIMAL = { num_fmt: 44 }.freeze   # Accounting: 44
-      US_DOLLAR_5_DECIMAL = { format_code: '_(&quot;$&quot;* #,##0.00000_);;_(@_)' }.freeze
+      US_DOLLAR_5_DECIMAL = { format_code: '$* #,##0.00???' }.freeze
       PERCENT_2_DECIMAL = { num_fmt: 10 }.freeze     # 0 Decimal: 9, 2 Decimal: 10
       ALIGN_RIGHT = { alignment: { horizontal: :right } }.freeze
-      DATE_FORMAT = { format_code: 'mmm\, dd' }.freeze
+      DATE_FORMAT = { format_code: 'mmm dd, yyyy' }.freeze
 
       def generate(sheet)
         @styles = {
@@ -18,7 +18,7 @@ module XLSX
           percent: add_style(sheet, BASE_FORMAT, PERCENT_2_DECIMAL, ALIGN_RIGHT),
           money: add_style(sheet, BASE_FORMAT, US_DOLLAR_2_DECIMAL, ALIGN_RIGHT),
           money5: add_style(sheet, BASE_FORMAT, US_DOLLAR_5_DECIMAL, ALIGN_RIGHT),
-          date: add_style(sheet, DATE_FORMAT, ALIGN_RIGHT),
+          date: add_style(sheet, BASE_FORMAT, DATE_FORMAT, ALIGN_RIGHT),
           header: add_style(sheet, BASE_FORMAT, HEADER_FORMAT),
           header_money: add_style(sheet, BASE_FORMAT, HEADER_FORMAT, US_DOLLAR_2_DECIMAL, ALIGN_RIGHT),
           header_right: add_style(sheet, BASE_FORMAT, HEADER_FORMAT, ALIGN_RIGHT),

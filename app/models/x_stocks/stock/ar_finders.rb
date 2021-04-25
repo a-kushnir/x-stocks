cdc240d4d4a0e8ef0bf8f401407a1e51ca1e2da0
Andrew Kushnir <akushnir2009@gmail.com>
2021-04-24 20:33:57 -0600

    Price Range

--- app/models/x_stocks/stock/ar_finders.rb
+++ app/models/x_stocks/stock/ar_finders.rb
@@ -5,12 +5,7 @@ module XStocks
     # Stock AR Finders Business Model
     module ARFinders
       def find_all_random
-        stock_ids = XStocks::AR::Position.distinct.pluck(:stock_id)
-
-        stocks = XStocks::AR::Stock.random.where(id: stock_ids).all.to_a
-        stocks += XStocks::AR::Stock.random.where.not(id: stock_ids).all.to_a
-
-        stocks.map { |ar_stock| XStocks::Stock.new(ar_stock) }
+        stocks_in_random_order.map { |ar_stock| XStocks::Stock.new(ar_stock) }
       end
 
       def find_all(*args)
@@ -30,6 +25,18 @@ module XStocks
       def find_by_symbol(symbol)
         ar_stock = XStocks::AR::Stock.find_by(symbol: symbol.to_s.upcase)
         XStocks::Stock.new(ar_stock) if ar_stock
+      end
+
+      private
+
+      def stocks_in_random_order
+        if Random.new.rand(100) <= 66
+          stock_ids = XStocks::AR::Position.distinct.pluck(:stock_id)
+          XStocks::AR::Stock.random.where(id: stock_ids).all.to_a +
+            XStocks::AR::Stock.random.where.not(id: stock_ids).all.to_a
+        else
+          XStocks::AR::Stock.random.all.to_a
+        end
       end
     end
   end
--- app/models/x_stocks/stock/calculator.rb
+++ app/models/x_stocks/stock/calculator.rb
@@ -5,8 +5,10 @@ module XStocks
     # Stock Calculator Business Model
     module Calculator
       def calculate_stock_prices
-        ar_stock.week_52_high = [ar_stock.current_price, ar_stock.week_52_high].compact.max if ar_stock.week_52_high
-        ar_stock.week_52_low = [ar_stock.current_price, ar_stock.week_52_low].compact.min if ar_stock.week_52_low
+        ar_stock.day_high_price = [ar_stock.current_price, ar_stock.day_high_price].compact.max if ar_stock.day_high_price
+        ar_stock.day_low_price = [ar_stock.current_price, ar_stock.day_low_price].compact.min if ar_stock.day_low_price
+        ar_stock.week_52_high = [ar_stock.current_price, ar_stock.day_high_price, ar_stock.week_52_high].compact.max if ar_stock.week_52_high
+        ar_stock.week_52_low = [ar_stock.current_price, ar_stock.day_low_price, ar_stock.week_52_low].compact.min if ar_stock.week_52_low
         ar_stock.price_change = safe_exec { ar_stock.current_price - ar_stock.prev_close_price }
         ar_stock.price_change_pct = safe_exec { ar_stock.price_change / ar_stock.prev_close_price * 100 }
         ar_stock.market_capitalization = safe_exec { ar_stock.outstanding_shares * ar_stock.current_price }

d2346e45b253f973b934f1f06671265623314b20
Andrew Kushnir <akushnir2009@gmail.com>
2022-03-09 03:57:41 -0700

    Tailwind

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,8 @@
 module.exports = {
   plugins: [
     require('postcss-import'),
+    require('tailwindcss/nesting'),
+    require('tailwindcss'),
     require('postcss-flexbugs-fixes'),
     require('postcss-preset-env')({
       autoprefixer: {

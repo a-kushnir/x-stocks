d85ae187c6863b3322f35914afeabdcaa0152358
Andrew Kushnir <akushnir2009@gmail.com>
2022-03-09 22:38:35 -0700

    Tailwind

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,6 +2,8 @@
 
 # Controller to provide user sign in/out functions
 class SessionsController < Devise::SessionsController
+  layout 'application_old'
+
   # POST /resource/sign_in
   def create
     self.resource = warden.authenticate!(auth_options)

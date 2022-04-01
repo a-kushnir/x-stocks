c67ee58e41ac34d04c83fe5aacbe8efd40cfbb1c
Andrew Kushnir <akushnir2009@gmail.com>
2022-04-01 01:52:17 -0600

    Taxation form

--- app/models/x_stocks/stock/taxes.rb
+++ app/models/x_stocks/stock/taxes.rb
@@ -7,7 +7,7 @@ module XStocks
       TAXES = {
         foreign_tax: ['Foreign Tax', nil, 'svg/earth', 'tag tag-orange'],
         schedule_k1: ['Schedule K-1 (Form 1065)', 'https://www.irs.gov/forms-pubs/about-schedule-k-1-form-1065', 'svg/file-document-multiple-outline', 'tag tag-red'],
-        ordinary_divs: ['Ordinary Dividends', nil, 'svg/cash-multiple', 'tag tag-red'],
+        ordinary_divs: ['Non-Qualified Dividend', nil, 'svg/cash-multiple', 'tag tag-red'],
         qualified_divs: ['Qualified Dividends', 'https://en.wikipedia.org/wiki/Qualified_dividend', 'svg/cash-multiple', 'tag tag-green']
       }.freeze
     end

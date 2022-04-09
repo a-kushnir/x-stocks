0bb47cd46de6ba0562834e86f2a6f7189ffe1d49
Andrew Kushnir <akushnir2009@gmail.com>
2022-04-08 23:56:37 -0600

    Honeybadger

---
+++ config/initializers/honeybadger.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require 'honeybadger'
+
+::Honeybadger.configure do |config|
+  config.api_key = ENV['HONEYBADGER_API_KEY'].presence
+  config.send_data_at_exit = false
+  config.env = Rails.env
+  config.report_data = (config.api_key && Rails.env.production? ? true : false)
+end

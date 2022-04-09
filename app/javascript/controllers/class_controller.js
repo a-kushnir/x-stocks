f7d05e49339d30a8c54fe563fa40542a43afb62a
Andrew Kushnir <akushnir2009@gmail.com>
2022-04-09 04:02:10 -0600

    Columns menu

--- app/javascript/controllers/class_controller.js
+++ app/javascript/controllers/class_controller.js
@@ -3,7 +3,7 @@ import ApplicationController from "controllers/application_controller";
 export default class extends ApplicationController {
   static targets = [ "element" ]
 
-  toggle(event) {
+  toggle() {
     this.elementTargets.forEach((element) => {
       element.classList.toggle(element.dataset.classValue);
     });

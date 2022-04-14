332ca5902447764a153e7298a8c50de7072acd4d
Andrew Kushnir <akushnir2009@gmail.com>
2022-04-14 02:56:39 -0600

    Javascript migration

--- app/javascript/controllers/tradingview_chart_controller.js
+++ app/javascript/controllers/tradingview_chart_controller.js
@@ -9,21 +9,23 @@ export default class extends ApplicationController {
       this.element.id = generateId();
     }
 
-    new TradingView.widget({
-      "autosize": true,
-      "symbol": this.symbolValue,
-      "timezone": "Etc/UTC",
-      "theme": "light",
-      "style": "2",
-      "locale": "en",
-      "toolbar_bg": "#f1f3f6",
-      "enable_publishing": false,
-      "hide_top_toolbar": true,
-      "withdateranges": true,
-      "range": "1m",
-      "allow_symbol_change": true,
-      "save_image": false,
-      "container_id": this.element.id
+    import("https://s3.tradingview.com/tv.js").then(() => {
+      new TradingView.widget({
+        "autosize": true,
+        "symbol": this.symbolValue,
+        "timezone": "Etc/UTC",
+        "theme": "light",
+        "style": "2",
+        "locale": "en",
+        "toolbar_bg": "#f1f3f6",
+        "enable_publishing": false,
+        "hide_top_toolbar": true,
+        "withdateranges": true,
+        "range": "1m",
+        "allow_symbol_change": true,
+        "save_image": false,
+        "container_id": this.element.id
+      });
     });
   }
 }

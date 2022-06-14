4142560d615b5e721bcf82504f2ad4e43948ff23
Andrew Kushnir <akushnir2009@gmail.com>
2022-06-14 00:45:40 -0600

    Add element#scrollTo

--- app/javascript/controllers/element_controller.js
+++ app/javascript/controllers/element_controller.js
@@ -4,4 +4,10 @@ export default class extends ApplicationController {
   requestSubmit() {
     this.element.form.requestSubmit();
   }
+
+  scrollTo() {
+    let target = this.element.dataset.elementScrollTarget;
+    target = document.getElementById(target);
+    target.scrollIntoView({ behavior: 'smooth' }); // { behavior: 'auto' }
+  }
 }

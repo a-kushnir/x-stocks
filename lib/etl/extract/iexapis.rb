ee2839c4095b6429100a21a3fa355b4b4c8bdf2a
Andrew Kushnir <akushnir2009@gmail.com>
2022-10-21 23:57:10 -0600

    Added VettaFi

--- lib/etl/extract/iexapis.rb
+++ lib/etl/extract/iexapis.rb
@@ -17,50 +17,10 @@ module Etl
         data_loader.get_json(company_url(stock.symbol))
       end
 
-      def dividends(stock)
-        data_loader.get_json(dividends_url(stock.symbol))
-      end
-
-      def dividends_1m(stock)
-        data_loader.get_json(dividends_1m_url(stock.symbol))
-      end
-
-      def dividends_3m(stock)
-        data_loader.get_json(dividends_3m_url(stock.symbol))
-      end
-
-      def dividends_6m(stock)
-        data_loader.get_json(dividends_6m_url(stock.symbol))
-      end
-
-      def dividends_next(stock)
-        data_loader.get_json(dividends_next_url(stock.symbol))
-      end
-
       private
 
       def company_url(symbol)
         "#{BASE_URL}/stock/#{cgi.escape(symbol)}/company?token=#{token}"
-      end
-
-      def dividends_url(symbol)
-        "#{BASE_URL}/stock/#{cgi.escape(symbol)}/dividends?token=#{token}"
-      end
-
-      def dividends_1m_url(symbol)
-        "#{BASE_URL}/stock/#{cgi.escape(symbol)}/dividends/1m?token=#{token}"
-      end
-
-      def dividends_3m_url(symbol)
-        "#{BASE_URL}/stock/#{cgi.escape(symbol)}/dividends/3m?token=#{token}"
-      end
-
-      def dividends_6m_url(symbol)
-        "#{BASE_URL}/stock/#{cgi.escape(symbol)}/dividends/6m?token=#{token}"
-      end
-
-      def dividends_next_url(symbol)
-        "#{BASE_URL}/stock/#{cgi.escape(symbol)}/dividends/next?token=#{token}"
       end
 
       attr_reader :data_loader, :token, :cgi
---
+++ lib/etl/extract/vetta_fi.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+module Etl
+  module Extract
+    # Extracts data from VettaFi (etfdb.com)
+    class VettaFi
+      BASE_URL = 'https://etfdb.com'
+
+      def initialize(data_loader, cgi: CGI)
+        @data_loader = data_loader
+        @cgi = cgi
+      end
+
+      def etf_data(stock)
+        return unless stock.etf?
+
+        fields = {
+          website: -> (html) { html.scan(%r{<span>ETF Home Page<\/span>\n<span class='truncate-index'><a href="([^>]+)">Home page<\/a><\/span>})&.dig(0, 0) },
+          expense_ratio: -> (html) { html.scan(%r{<span>Expense Ratio<\/span>\n<span class='text-right'>(\d.\d+)%<\/span>})&.dig(0, 0)&.to_f },
+        }
+
+        html = data_loader.get_text(etf_data_url(stock.symbol))
+        fields.transform_values { |proc| proc.call(html) }
+      end
+
+      private
+
+      def etf_data_url(symbol)
+        "#{BASE_URL}/etf/#{cgi.escape(symbol)}/"
+      end
+
+      attr_reader :data_loader, :token, :cgi
+    end
+  end
+end

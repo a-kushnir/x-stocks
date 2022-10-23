ad8688e8c228161d410eef250c20506d76fcf54d
Andrew Kushnir <akushnir2009@gmail.com>
2022-10-23 04:26:14 -0600

    Fix back_url

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,12 +4,9 @@ require 'x_stocks'
 
 # Controller to provide user sign in/out functions
 class SessionsController < Devise::SessionsController
-  # POST /resource/sign_in
-  def create
-    self.resource = warden.authenticate!(auth_options)
-    set_flash_message!(:notice, :signed_in)
-    sign_in(resource_name, resource)
-    yield resource if block_given?
-    respond_with resource, location: params[:back_url] || after_sign_in_path_for(resource)
+  private
+
+  def after_sign_in_path_for(_resource)
+    params[:back_url] || super
   end
 end

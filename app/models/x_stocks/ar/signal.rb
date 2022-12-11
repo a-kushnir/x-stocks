ef0c3ea0f5a1deb3feaba4f4df5c842f2e606d2e
Andrew Kushnir <akushnir2009@gmail.com>
2022-12-11 05:32:31 -0700

    Add Signals on Stock Details

--- app/models/x_stocks/ar/signal.rb
+++ app/models/x_stocks/ar/signal.rb
@@ -7,8 +7,20 @@ module XStocks
       belongs_to :stock
 
       validates :timestamp, :detection_method, :value, :price, presence: true
+      validates :value, inclusion: { in: %w(buy sell) }
 
       default_scope { order(timestamp: :desc) }
+
+      scope :buy, -> { where(value: 'buy') }
+      scope :sell, -> { where(value: 'sell') }
+
+      def buy?
+        value == 'buy'
+      end
+
+      def sell?
+        value == 'buy'
+      end
     end
   end
 end
